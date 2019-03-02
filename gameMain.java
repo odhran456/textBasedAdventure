f3d9179064ec3e28f6b4a8455e5b19b56a137b20
odhran456 <44904580+odhran456@users.noreply.github.com>
2019-03-02 00:09:54 +0000

    Create gameMain.java

---
+++ gameMain.java
@@ -0,0 +1,37 @@
+public class gameMain{	
+
+	public static void main(String[] args) {
+		
+		headSlot bronzeMedHelm = new headSlot("Bronze Open Faced Helmet",1,0,0,3,0,20,"A rather pathetic excuse for a helmet");
+    	chestSlot bronzePlateBody = new chestSlot("Bronze Plate Body",3,0,3,0,0,55,"Provides poor protection for the body");
+    	headSlot beret = new headSlot("White Beret",0,0,0,8,0,65,"A fashionable plain white beret");
+    	chestSlot womensTunic = new chestSlot("Women's Tunic",0,0,0,5,0,50,"A slim fitting, white button up women's tunic");
+    	headSlot hatGoggles = new headSlot("Wooly Hat and Goggles",0,0,0,3,2,50,"A practical headpiece for warmth and sunlight protection");
+    	chestSlot duster = new chestSlot("Duster Coat",0,0,0,6,2,100,"A long brown coat");
+    	
+    	
+    	itemList inventory = new itemList();
+    	
+    	npc Holo = new npc(beret,womensTunic,10,"Holo of Yoitsu","Holo");
+    	npc Kino = new npc(hatGoggles,duster,12,"Kino the Traveller","Kino");
+    	character me = new character(bronzeMedHelm,bronzePlateBody,10,Holo,inventory);
+    	
+    	me.setHitpoints(12);
+    	Kino.setHitpoints(14);
+    	
+    	me.setAlly(Kino);
+    	
+    	
+    	inventory.add(beret);
+    	inventory.add(duster);
+    	inventory.add(hatGoggles);
+    	inventory.add(bronzePlateBody);
+    	System.out.println(inventory.printItems());
+    	
+    	System.out.println(inventory.printStatsItem());
+		
+    	
+    	
+    	new gameGUI();
+	}
+}
