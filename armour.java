e376da4cd37c66de35e0b308f2eb1edacab0ba5d
odhran456 <44904580+odhran456@users.noreply.github.com>
2019-03-02 00:07:16 +0000

    Create armour.java

---
+++ armour.java
@@ -0,0 +1,35 @@
+
+public class armour extends item{
+	private String slotName;
+	protected double meleeDefence;
+	protected double magicDefence;
+	protected double rangeDefence;
+	protected double fashion;
+	protected double intimidating;
+	protected String details;
+	
+	
+	
+	public String getSlotName() {
+		return this.slotName;
+	}
+	public double getMeleeDefence() {
+		return this.meleeDefence;
+	}
+	public double getMagicDefence() {
+		return this.magicDefence;
+	}
+	public double getRangeDefence() {
+		return this.rangeDefence;
+	}
+	public double getFashion() {
+		return this.fashion;
+	}
+	public double getIntimidating() {
+		return this.intimidating;
+	}
+	public String getDetails() {
+		return this.details;
+	}
+		
+}
