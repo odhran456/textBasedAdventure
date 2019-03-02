a64d4f91adb78abb690842b47038150916942603
odhran456 <44904580+odhran456@users.noreply.github.com>
2019-03-02 00:10:49 +0000

    Create item.java

---
+++ item.java
@@ -0,0 +1,21 @@
+
+public class item {
+	protected double value;
+	private String itemName;
+	private String details;
+	private String examine;
+	
+	public double getValue() {
+		return this.value;
+	}
+	public String getItemName() {
+		return this.itemName;
+	}
+	public String getDetails() {
+		return this.details;
+	}
+	
+	public String getExamine() {
+		return this.examine;
+	}
+}
