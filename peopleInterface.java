415dce59193000868714ba03b82f431098d3b119
odhran456 <44904580+odhran456@users.noreply.github.com>
2019-03-02 00:12:15 +0000

    Create peopleInterface.java

---
+++ peopleInterface.java
@@ -0,0 +1,14 @@
+public interface peopleInterface{
+	public String getHeadSlot();
+	public String getChestSlot();
+	public double totalMeleeDefence();
+	public double totalMagicDefence();
+	public double totalRangeDefence();
+	public double totalFashion();
+	public double totalIntimidating();
+	//public double hitpoints();
+	public double getHitpoints();
+	public void setHeadSlot(headSlot item);
+	public void setChestSlot (chestSlot item);
+	
+}
